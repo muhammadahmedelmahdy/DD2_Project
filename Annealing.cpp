9e04c71a84204a3afcb7a1295ee2eec0923f0688
mostafa0001-me <57318849+mostafa0001-me@users.noreply.github.com>
2023-12-11 09:26:42 +0200

    Optimized Annealing

--- Annealing.cpp
+++ Annealing.cpp
@@ -23,8 +23,6 @@ struct Cell {
 };
 
 class placer {
-
-   // constructor for the placer object that is responsible for doing the initial random placement and calculating the initial hpwl
    public: placer(string filename) {
       parseInput(filename, totalcomponents);
       totalHPWL = 0;
@@ -32,26 +30,22 @@ class placer {
       initialPlacement(totalcomponents);
       auto stop = high_resolution_clock::now();
       auto duration = duration_cast < microseconds > (stop - start);
-      printPlacement();
+      printFinalPlacement();
       cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
       calculateInitialHPWL();
 
    }
 
-   // Run function is reponsible for calling the functions needed for annealing in addition to calculating the annwaling time
    void run() {
       auto start = high_resolution_clock::now();
       annealing();
       auto stop = high_resolution_clock::now();
       auto duration = duration_cast < microseconds > (stop - start);
       cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
-      printPlacement();
-      cout << endl <<  "Binary representation: " << endl << endl;
-      printBinarylPlacement();
+      printFinalPlacement();
    }
 
-   private: 
-   int numRows,
+   private: int numRows,
    numColumns;
    int totalcomponents,
    totalnets;
@@ -63,15 +57,15 @@ class placer {
    HPWL_X_I,
    HPWL_Y_I2,
    HPWL_X_I2;
+   vector < vector < Cell * >> netlist_y;
    vector < vector < int >> grid;
    int totalHPWL;
 
-   // Function to parse the netlist file and intialize the private variables with the values needed for the annealing
    void parseInput(const string filename, int & totalcomponents) {
       ifstream netfile(filename);
       if (!netfile.is_open()) {
          cout << "File is not open" << endl;
-         exit(0);
+         return;
       }
       netfile >> totalcomponents >> totalnets >> numRows >> numColumns;
       grid.resize(numRows);
@@ -89,6 +83,7 @@ class placer {
       }
 
       netlist.reserve(totalnets);
+      netlist_y.reserve(totalnets);
       HPWL_Y_I.reserve(totalnets);
       HPWL_X_I.reserve(totalnets);
       HPWL_Y_I2.reserve(totalnets);
@@ -106,11 +101,11 @@ class placer {
             net.push_back(temp);
          }
          netlist.push_back(net);
+         netlist_y.push_back(net);
       }
       netfile.close();
    }
 
-   // Function to do the initial placement of the components in the 2D grid
    void initialPlacement(int numTotalComponents) {
       vector < int > cellFills(numTotalComponents);
       generate(cellFills.begin(), cellFills.end(), [n = 0]() mutable {
@@ -141,37 +136,29 @@ class placer {
       }
    }
 
-   // Comparator function to help sort cells according to the values of x 
    static bool compare_X_coordinate(Cell * A, Cell * B) {
       return A -> x < B -> x;
    }
 
-   // Comparator function to help sort cells according to the values of y
    static bool compare_Y_coordinate(Cell * A, Cell * B) {
       return A -> y < B -> y;
    }
 
-   // Function to calculate the x component of HPWL to a specific net
    int calculateHPWL_X(vector < Cell * > & X) {
-      Cell* Min = min_element(X.begin(), X.end(), compare_X_coordinate)[0];
-      Cell* Max = max_element(X.begin(), X.end(), compare_X_coordinate)[0];
-      int dx = Max -> x - Min -> x;
+      sort(X.begin(), X.end(), compare_X_coordinate);
+      int dx = X.back() -> x - X.front() -> x;
       return dx;
    }
-
-   // Function to calculate the y component of HPWL to a specific net
    int calculateHPWL_Y(vector < Cell * > & Y) {
-      Cell* Min = min_element(Y.begin(), Y.end(), compare_Y_coordinate)[0];
-      Cell* Max = max_element(Y.begin(), Y.end(), compare_Y_coordinate)[0];
-      int dy = Max -> y - Min -> y;
+      sort(Y.begin(), Y.end(), compare_Y_coordinate);
+      int dy = Y.back() -> y - Y.front() -> y;
       return dy;
    }
 
-   // Function to calculate the initial total HPWL and to intilize the values of the x and y components of HPWL to each net
    void calculateInitialHPWL() {
       for (int i = 0; i < netlist.size(); i++) {
          int temp1 = calculateHPWL_X(netlist[i]);
-         int temp2 = calculateHPWL_Y(netlist[i]);
+         int temp2 = calculateHPWL_Y(netlist_y[i]);
          HPWL_X.push_back(temp1);
          HPWL_Y.push_back(temp2);
          HPWL_X_I.push_back(temp1);
@@ -185,17 +172,23 @@ class placer {
       }
    }
 
-   // Function to check is the x component of an hpwl of a specific net need to be changed because of the change of the location of a specific cell
    void checkHPWL_X(const Cell * cell, int net, int c) {
       if (cell == netlist[net].front()) {
+         //if (cell -> x < netlist[net][1] -> x) {
             totalHPWL -= HPWL_X[net];
             HPWL_X[net] = calculateHPWL_X(netlist[net]);
             totalHPWL += HPWL_X[net];
-
+        // } else {
+         //   return;
+         //}
       } else if (cell == netlist[net].back()) {
+         //if (cell -> x > netlist[net][netlist[net].size() - 2] -> x) {
             totalHPWL -= HPWL_X[net];
             HPWL_X[net] = calculateHPWL_X(netlist[net]);
             totalHPWL += HPWL_X[net];
+         //} else {
+         //   return;
+         //}
       } else {
          if (cell -> x > netlist[net].back() -> x || cell -> x < netlist[net].front() -> x) {
             totalHPWL -= HPWL_X[net];
@@ -207,21 +200,27 @@ class placer {
       }
    }
 
-   // Function to check is the y component of an hpwl of a specific net need to be changed because of the change of the location of a specific cell
    void checkHPWL_Y(const Cell * cell, int net, int c) {
-      if (cell == netlist[net].front()) {
+      if (cell == netlist_y[net].front()) {
+         //if (cell -> y < netlist_y[net][1] -> y) {
             totalHPWL -= HPWL_Y[net];
-            HPWL_Y[net] = calculateHPWL_Y(netlist[net]);
+            HPWL_Y[net] = calculateHPWL_Y(netlist_y[net]);
             totalHPWL += HPWL_Y[net];
-
-      } else if (cell == netlist[net].back()) {
+         //} else {
+            //return;
+         //}
+      } else if (cell == netlist_y[net].back()) {
+         //if (cell -> y > netlist_y[net][netlist_y[net].size() - 2] -> y) {
             totalHPWL -= HPWL_Y[net];
-            HPWL_Y[net] = calculateHPWL_Y(netlist[net]);
+            HPWL_Y[net] = calculateHPWL_Y(netlist_y[net]);
             totalHPWL += HPWL_Y[net];
+         //} else {
+         //   return;
+         //}
       } else {
-         if (cell -> y > netlist[net].back() -> y || cell -> y < netlist[net].front() -> y) {
+         if (cell -> y > netlist_y[net].back() -> y || cell -> y < netlist_y[net].front() -> y) {
             totalHPWL -= HPWL_Y[net];
-            HPWL_Y[net] = calculateHPWL_Y(netlist[net]);
+            HPWL_Y[net] = calculateHPWL_Y(netlist_y[net]);
             totalHPWL += HPWL_Y[net];
          } else {
             return;
@@ -229,18 +228,20 @@ class placer {
       }
    }
 
-   // Function to check if the swap of two cells will be accepted by checking the total hpwl
    bool checker(const int IHPWL1,
       const Cell * X,
          const Cell * Y, double temperature) {
+      //cout << IHPWL1 << "  " << totalHPWL << "\n";
       if (X != NULL) {
          for (int i = 0; i < X -> nets.size(); i++) {
+            //cout << "HERE!";
             HPWL_X_I[X -> nets[i]] = HPWL_X[X -> nets[i]];
             HPWL_Y_I[X -> nets[i]] = HPWL_Y[X -> nets[i]];
          }
       }
       if (Y != NULL) {
          for (int i = 0; i < Y -> nets.size(); i++) {
+            //cout << "HERE1!";
             HPWL_X_I2[Y -> nets[i]] = HPWL_X[Y -> nets[i]];
             HPWL_Y_I2[Y -> nets[i]] = HPWL_Y[Y -> nets[i]];
          }
@@ -258,28 +259,41 @@ class placer {
             checkHPWL_Y(Y, Y -> nets[i], 1);
          }
       }
+      //cout << totalHPWL << "  " << HPWL_X[7] << HPWL_Y[7] << "  " << components[7]->x << " " << components[7]->y << " " << components[13]->x << " " << components[13]->y << "  " << IHPWL1 << "\n";
+      //std::srand(std::time(0));   
       int deltaCost = IHPWL1 - totalHPWL;
+      //cout << IHPWL1 << "  " << totalHPWL << "\n";
       double random_number = static_cast < double > (std::rand()) / RAND_MAX;
+      //cout << random_number << "\n";
       double e = 1 - exp(static_cast < double > (deltaCost) / temperature);
       bool check = ((deltaCost < 0 || deltaCost == 0) && (random_number) < e);
+ /*     cout << deltaCost << "  " << temperature << "  " << random_number << "  " << e << "  " << check << "\t";
+     for(int i = 0; i < HPWL_X.size(); i++){
+     	cout << HPWL_X[i] << "  " << HPWL_Y[i] << "\t";
+     }
+     cout << "\n";*/
       return check;
    }
 
-   // Annealing function is responsible for the annealing process by swaping to random cells and checking if the swap is accepted or not
    void annealing() {
+      
       cout << "started annealing" << endl;
       double initialCost = totalHPWL;
       double initialTemp = initialCost * 500;
       double finalTemp = 5 * pow(10, -6) * (initialCost / totalnets);
       double currentTemp = initialCost * 500;
       int IHPWL = initialCost;
+      int THPWL = initialCost;
+      //srand(time(0));
       while (currentTemp > finalTemp) {
          int x_temp1, x_temp2, y_temp1, y_temp2;
          for (int i = 0; i < 10 * numRows * numColumns; ++i) {
+            //srand(time(0));
             x_temp1 = rand() % numRows;
             x_temp2 = rand() % numRows;
             y_temp1 = rand() % numColumns;
             y_temp2 = rand() % numColumns;
+            //cout << x_temp1 << "  " << x_temp2 << "    " << y_temp1 << "  " << y_temp2 << "\n" ;
 
             Cell * X = NULL;
             Cell * Y = NULL;
@@ -299,6 +313,7 @@ class placer {
             }
             IHPWL = totalHPWL;
             bool checkk = checker(IHPWL, X, Y, currentTemp);
+            //cout << checkk << "  " << currentTemp <<"\n";
             if (checkk == 0) {
                grid[x_temp1][y_temp1] = comp2;
                grid[x_temp2][y_temp2] = comp1;
@@ -322,37 +337,32 @@ class placer {
                totalHPWL = IHPWL;
 
             }
+            //printFinalPlacement();
+            //cout << totalHPWL << "  " << IHPWL << "  " << currentTemp << "  " << checkk << "\n";
          }
+         //printFinalPlacement();
+         //cout << totalHPWL << "  " << currentTemp << "\n";
          currentTemp *= 0.95;
+         if(totalHPWL == THPWL)
+             return;
+         THPWL = totalHPWL;
+         //cout << totalHPWL << "\n";
       }
 
       cout << "Initial cost: " << initialCost << endl;
       cout << "Final Cost: " << totalHPWL << endl;
    }
 
-   // Print the 2D grid with the componenets names
-   void printPlacement() const {
+   void printFinalPlacement() const {
       for (const auto & row: grid) {
          for (int cell: row) {
             if (cell == -1) {
-               cout << "----" << "\t";
+               cout << "-" << "\t";
+               //cout << "0";
             } else {
                cout << setw(4) << setfill('0') << cell << "\t";
-            }
-         }
-         cout << "\n";
-      }
-
-   }
-
-   // Print the 2D grid with a binary representation of 0 to empty cells and 1 to occupied cells
-   void printBinarylPlacement() const {
-      for (const auto & row: grid) {
-         for (int cell: row) {
-            if (cell == -1) {
-               cout << 0;
-            } else {
-               cout << 1;
+               //cout << components[cell]->x << components[cell]->y << "\t";
+               //cout << "1";
             }
          }
          cout << "\n";
@@ -361,18 +371,16 @@ class placer {
    }
 };
 
-// Check if there is an error in the main parameters
-void emitError(string s) {
+void emitError(char * s) {
    cout << s;
    exit(0);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[]) {
 
    if (argc < 2) emitError("use: placer <netlist_file_name>\n");
-   string filename = "Test Cases/" + string(argv[1]);
-   placer place(filename);
+   srand(time(0));
+   placer place(argv[1]);
    place.run();
    return 0;
 }
- 
